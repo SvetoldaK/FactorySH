81f14b82e1fce4c2ad02d47b49b76d2db07778ec
SvetoldaK <svetakurilova2001@mail.ru>
2021-11-12 23:56:40 +0300

    sh

---
+++ src/main/java/com/githab/SvetoldaK/Factory.java
@@ -0,0 +1,16 @@
+package com.githab.SvetoldaK;
+
+public class Factory {
+    SuperHero batman = new SuperHero();
+    /**
+     * тут я хочу типа
+     * SuperHero batman = new SuperHero;
+     * batman.setName("Betman");
+     * batman.setForce(6);
+     * потом
+     * SuperHero hulk = new SuperHero;
+     * hulk.setName("Hulk");
+     * hulk.setForce(6);
+     */
+
+}
---
+++ src/main/java/com/githab/SvetoldaK/SuperHero.java
@@ -0,0 +1,72 @@
+package com.githab.SvetoldaK;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+
+public class SuperHero {
+    private String name;
+    private int force;
+    private boolean universe;
+    private String superpower;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getForce() {
+        return force;
+    }
+
+    public void setForce(int force) {
+        this.force = force;
+    }
+
+    public boolean isUniverse() {
+        return universe;
+    }
+
+    public void setUniverse(boolean marvel) {
+        this.universe = marvel;
+    }
+
+    public String getSuperpower() {
+        return superpower;
+    }
+
+    public void setSuperpower(String superpower) {
+        this.superpower = superpower;
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof SuperHero)) return false;
+        SuperHero hero = (SuperHero) o;
+        return force == hero.force &&
+                universe == hero.universe &&
+                superpower == hero.superpower &&
+                Objects.equals(name, hero.name);
+    }
+
+    public int hashCode() {
+        return Objects.hash(name, force, universe, superpower);
+    }
+
+    public String toString() {
+        return "Factory{" +
+                "name='" + name + '\'' +
+                ", force=" + force +
+                ", universe=" + universe +
+                ", superpower=" + superpower +
+                '}';
+    }
+
+
+}
